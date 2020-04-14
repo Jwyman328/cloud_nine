e8ed55aa90f427440efdd9366fb6154d4cda5cbb
Joseph Wyman <jwymandev@gmail.com>
2020-04-14 19:16:59 -0400

    Add custom hook to tour chart

--- src/components/TourChart.js
+++ src/components/TourChart.js
@@ -3,31 +3,15 @@ import TourItem from './TourItem'
 import makeTimeDataReadable from '../helperfunctions/makeTimeDataReadable';
 import createTourChart from '../helperfunctions/createTourChart';
 import isAPIDataLoaded from "../helperfunctions/isAPIDataLoaded";
+import useFetchAndSetAPIData from '../customhooks/useFetchAndSetAPIData'
 
 function TourChart(props) {
-    const [showItems, setShowItems] = useState([])
 
-    const fetchShows =  async() => {
-        const data = await fetch('https://cloudninebackend2.herokuapp.com/current_tour_shows/')
-        const dataJson = await data.json()
-        setShowItems(dataJson)
-      }
+ 
+    const showItems = useFetchAndSetAPIData('https://cloudninebackend2.herokuapp.com/current_tour_shows/')
 
-      useEffect(() => {
-        fetchShows()
-    }, [])
-
-/*     const createTourChart = () => {
-        const allTourItems = showItems.map((showItem) => {
-            const [hourTime, dateTime] = makeTimeDataReadable(showItem.date)
-            return  <TourItem date={dateTime} time={hourTime} location = {showItem.location} venue={showItem.venue} />
-        })
-        return allTourItems
-    } */
   return (
-    <div className="tour-chart">
-{/*         <TourItem date='the date' location = 'MA' venue=',y house' />
- */}    
+    <div className="tour-chart">  
     {isAPIDataLoaded(showItems)? createTourChart(showItems) : <div className='noShows'>No Shows Scheduled </div> }
  </div>
   );
